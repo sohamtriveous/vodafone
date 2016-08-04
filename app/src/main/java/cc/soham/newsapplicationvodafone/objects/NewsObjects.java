95c649f024e91aeb5ccfe9f2c24734f0281e7a61
Soham Mondal <soham.mondal@gmail.com>
2016-08-04 12:49:16 +0530

    added more news objects to the static list

--- app/src/main/java/cc/soham/newsapplicationvodafone/objects/NewsObjects.java
+++ app/src/main/java/cc/soham/newsapplicationvodafone/objects/NewsObjects.java
@@ -97,6 +97,62 @@ public class NewsObjects {
                 "New York Governor signs daily fantasy sports bill, DraftKings and FanDuel can operate again",
                 "2016-08-04T04:20:46Z",
                 "Back in June on the last day of the New York Assembly's legislative session, the state passed a bill legalizing daily fantasy sports, paving the road for the",
+                "http://social.techcrunch.com/2016/08/03/new-york-governor-signs-daily-fantasy-sports-bill-draftkings-and-fanduel-can-operate-again/"));newsObjectsList.add(new NewsObjects("https://tctechcrunch2011.files.wordpress.com/2016/08/gettyimages-584268466.jpg?w=764&amp;h=400&amp;crop=1",
+                "New York Governor signs daily fantasy sports bill, DraftKings and FanDuel can operate again",
+                "2016-08-04T04:20:46Z",
+                "Back in June on the last day of the New York Assembly's legislative session, the state passed a bill legalizing daily fantasy sports, paving the road for the",
+                "http://social.techcrunch.com/2016/08/03/new-york-governor-signs-daily-fantasy-sports-bill-draftkings-and-fanduel-can-operate-again/"));
+
+        newsObjectsList.add(new NewsObjects("https://img.vidible.tv/prod/2016-08/03/57a274e1134aa15a39f04209_1280x720_U_v1_764_400.jpg",
+                "Crunch Report | First private company to go to the Moon",
+                "2016-08-04T03:00:38Z",
+                "Moon Express is the first private company to get permission from the U.S. government to go to the moon, Facebook shows us its \"Area 404\" hardware lab, Tesla.",
+                "http://social.techcrunch.com/2016/08/03/crunch-report-first-private-company-to-go-to-the-moon/"));
+
+        newsObjectsList.add(new NewsObjects("https://tctechcrunch2011.files.wordpress.com/2016/08/gettyimages-584268466.jpg?w=764&amp;h=400&amp;crop=1",
+                "New York Governor signs daily fantasy sports bill, DraftKings and FanDuel can operate again",
+                "2016-08-04T04:20:46Z",
+                "Back in June on the last day of the New York Assembly's legislative session, the state passed a bill legalizing daily fantasy sports, paving the road for the",
+                "http://social.techcrunch.com/2016/08/03/new-york-governor-signs-daily-fantasy-sports-bill-draftkings-and-fanduel-can-operate-again/"));
+
+        newsObjectsList.add(new NewsObjects("https://tctechcrunch2011.files.wordpress.com/2016/08/gettyimages-584268466.jpg?w=764&amp;h=400&amp;crop=1",
+                "New York Governor signs daily fantasy sports bill, DraftKings and FanDuel can operate again",
+                "2016-08-04T04:20:46Z",
+                "Back in June on the last day of the New York Assembly's legislative session, the state passed a bill legalizing daily fantasy sports, paving the road for the",
+                "http://social.techcrunch.com/2016/08/03/new-york-governor-signs-daily-fantasy-sports-bill-draftkings-and-fanduel-can-operate-again/"));
+
+        newsObjectsList.add(new NewsObjects("https://tctechcrunch2011.files.wordpress.com/2016/08/gettyimages-584268466.jpg?w=764&amp;h=400&amp;crop=1",
+                "New York Governor signs daily fantasy sports bill, DraftKings and FanDuel can operate again",
+                "2016-08-04T04:20:46Z",
+                "Back in June on the last day of the New York Assembly's legislative session, the state passed a bill legalizing daily fantasy sports, paving the road for the",
+                "http://social.techcrunch.com/2016/08/03/new-york-governor-signs-daily-fantasy-sports-bill-draftkings-and-fanduel-can-operate-again/"));newsObjectsList.add(new NewsObjects("https://tctechcrunch2011.files.wordpress.com/2016/08/gettyimages-584268466.jpg?w=764&amp;h=400&amp;crop=1",
+                "New York Governor signs daily fantasy sports bill, DraftKings and FanDuel can operate again",
+                "2016-08-04T04:20:46Z",
+                "Back in June on the last day of the New York Assembly's legislative session, the state passed a bill legalizing daily fantasy sports, paving the road for the",
+                "http://social.techcrunch.com/2016/08/03/new-york-governor-signs-daily-fantasy-sports-bill-draftkings-and-fanduel-can-operate-again/"));
+
+        newsObjectsList.add(new NewsObjects("https://img.vidible.tv/prod/2016-08/03/57a274e1134aa15a39f04209_1280x720_U_v1_764_400.jpg",
+                "Crunch Report | First private company to go to the Moon",
+                "2016-08-04T03:00:38Z",
+                "Moon Express is the first private company to get permission from the U.S. government to go to the moon, Facebook shows us its \"Area 404\" hardware lab, Tesla.",
+                "http://social.techcrunch.com/2016/08/03/crunch-report-first-private-company-to-go-to-the-moon/"));
+
+        newsObjectsList.add(new NewsObjects("https://tctechcrunch2011.files.wordpress.com/2016/08/gettyimages-584268466.jpg?w=764&amp;h=400&amp;crop=1",
+                "New York Governor signs daily fantasy sports bill, DraftKings and FanDuel can operate again",
+                "2016-08-04T04:20:46Z",
+                "Back in June on the last day of the New York Assembly's legislative session, the state passed a bill legalizing daily fantasy sports, paving the road for the",
+                "http://social.techcrunch.com/2016/08/03/new-york-governor-signs-daily-fantasy-sports-bill-draftkings-and-fanduel-can-operate-again/"));
+
+        newsObjectsList.add(new NewsObjects("https://tctechcrunch2011.files.wordpress.com/2016/08/gettyimages-584268466.jpg?w=764&amp;h=400&amp;crop=1",
+                "New York Governor signs daily fantasy sports bill, DraftKings and FanDuel can operate again",
+                "2016-08-04T04:20:46Z",
+                "Back in June on the last day of the New York Assembly's legislative session, the state passed a bill legalizing daily fantasy sports, paving the road for the",
+                "http://social.techcrunch.com/2016/08/03/new-york-governor-signs-daily-fantasy-sports-bill-draftkings-and-fanduel-can-operate-again/"));
+
+        newsObjectsList.add(new NewsObjects("https://tctechcrunch2011.files.wordpress.com/2016/08/gettyimages-584268466.jpg?w=764&amp;h=400&amp;crop=1",
+                "New York Governor signs daily fantasy sports bill, DraftKings and FanDuel can operate again",
+                "2016-08-04T04:20:46Z",
+                "Back in June on the last day of the New York Assembly's legislative session, the state passed a bill legalizing daily fantasy sports, paving the road for the",
                 "http://social.techcrunch.com/2016/08/03/new-york-governor-signs-daily-fantasy-sports-bill-draftkings-and-fanduel-can-operate-again/"));
     }
 }
