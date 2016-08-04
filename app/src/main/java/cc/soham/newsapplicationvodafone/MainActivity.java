c6929ab90e1158dde214674a555799bd83a4a8ab
Soham Mondal <soham.mondal@gmail.com>
2016-08-04 12:50:42 +0530

    added recyclerview and adapter, working local news home page

--- app/src/main/java/cc/soham/newsapplicationvodafone/MainActivity.java
+++ app/src/main/java/cc/soham/newsapplicationvodafone/MainActivity.java
@@ -2,9 +2,9 @@ package cc.soham.newsapplicationvodafone;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.widget.ProgressBar;
-import android.widget.TextView;
 
 import cc.soham.newsapplicationvodafone.objects.NewsObjects;
 
@@ -19,5 +19,9 @@ public class MainActivity extends AppCompatActivity {
 
         recyclerView = (RecyclerView) findViewById(R.id.activity_main_newsitems);
         progressBar = (ProgressBar) findViewById(R.id.activity_main_progress);
+
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        NewsAdapter newsAdapter = new NewsAdapter(NewsObjects.getNewsObjectsList());
+        recyclerView.setAdapter(newsAdapter);
     }
 }
