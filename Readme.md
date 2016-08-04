be5932507c618787555191b029db0ae24a536adf
Soham Mondal <soham.mondal@gmail.com>
2016-08-04 17:21:34 +0530

    Loading the second page from a cardview click

--- Readme.md
+++ Readme.md
@@ -1,4 +1,10 @@
 #things to do
 
-1. Create a static method in DetailsActivity for starting it
-2. Use this method inside the cardview onclicklistener to start it
+1. In the details activity get the position from the intent
+2. Based on the position get the NewsObject at that position
+3. Based on the news object
+a) change the title of the activity
+b) load the url (you have to initialize the webview)
+4. Create a horizontal progress bar in the details activity
+5. Show/Hide the progress bar when the page starts loading/finishes loading
+6. Implement "up functionality" in your details activity so that when you click on "up", you go back to the previous screen
--- app/src/main/java/cc/soham/newsapplicationvodafone/DetailsActivity.java
+++ app/src/main/java/cc/soham/newsapplicationvodafone/DetailsActivity.java
@@ -2,9 +2,16 @@ package cc.soham.newsapplicationvodafone;
 
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.ProgressBar;
+
+import cc.soham.newsapplicationvodafone.objects.NewsObjects;
 
 /**
  * Created by sohammondal on 04/08/16.
@@ -12,11 +19,42 @@ import android.support.v7.app.AppCompatActivity;
 
 public class DetailsActivity extends AppCompatActivity {
     private static final String KEY_POSITION = "position";
+    private static final int DEFAULT_POSITION = -1;
+
+    WebView webView;
+    ProgressBar progressBar;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_details);
+
+        webView = (WebView) findViewById(R.id.activity_details_webview);
+        progressBar = (ProgressBar) findViewById(R.id.activity_details_progress);
+
+        int position = getIntent().getIntExtra(KEY_POSITION, DEFAULT_POSITION);
+        NewsObjects newsObjects = NewsObjects.getNewsObjectsList().get(position);
+
+        loadWebViewForNewsObject(newsObjects);
+    }
+
+    private void loadWebViewForNewsObject(NewsObjects newsObjects) {
+        getSupportActionBar().setTitle(newsObjects.getTitle());
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.setWebViewClient(new WebViewClient() {
+            @Override
+            public void onPageStarted(WebView view, String url, Bitmap favicon) {
+                super.onPageStarted(view, url, favicon);
+                progressBar.setVisibility(View.VISIBLE);
+            }
+
+            @Override
+            public void onPageFinished(WebView view, String url) {
+                super.onPageFinished(view, url);
+                progressBar.setVisibility(View.GONE);
+            }
+        });
+        webView.loadUrl(newsObjects.getDetailsUrl());
     }
 
     public static void start(Context context, int position) {
